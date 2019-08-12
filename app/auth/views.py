ebb3f6c622ded864cf1f912a14f23fc029a3dd54
George Charles <charlesschiedel@gmail.com>
2019-08-12 22:34:23 +0300

    Defined another function for a route

--- app/auth/views.py
+++ app/auth/views.py
@@ -20,16 +20,16 @@ def login():
     title = "bloggy login"
     return render_template('auth/login.html',login_form = login_form,title=title)
 
-# @auth.route('/register',methods = ["GET","POST"])
-# def register():
-#     form = RegistrationForm()
-#     if form.validate_on_submit():
-#         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
-#         db.session.add(user)
-#         db.session.commit()
-#         title = "New Account"
-#         return redirect(url_for('auth.login'))
-#     return render_template('auth/register.html',registration_form = form)
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        title = "New Account"
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html',registration_form = form)
 
 # @auth.route('/logout')
 # @login_required
