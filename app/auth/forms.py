735f0b9aa4c78f5fdc13695c08335526a49c8038
George Charles <charlesschiedel@gmail.com>
2019-08-12 22:31:58 +0300

    Made functions inside my forms module

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -18,11 +18,11 @@ class RegistrationForm(FlaskForm):
     password_confirm = PasswordField('Confirm Password',validators = [Required()])
     submit = SubmitField('Sign Up')
 
-#     def validate_email(self,data_field):
-#             if User.query.filter_by(email =data_field.data).first():
-#                 raise ValidationError('There is an account with that email')
+    def validate_email(self,data_field):
+            if User.query.filter_by(email =data_field.data).first():
+                raise ValidationError('There is an account with that email')
 
-#     def validate_username(self,data_field):
-#         if User.query.filter_by(username = data_field.data).first():
-#             raise ValidationError('That username is taken')
+    def validate_username(self,data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError('That username is taken')
 
